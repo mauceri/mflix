9086e79deb484a934866cf63502730db4acd9093
Christian Mauceri <christian@mauceri.me>
2023-01-09 16:17:51 +0100

    Retour vers le futur

--- pages/movies.js
+++ pages/movies.js
@@ -25,7 +25,7 @@ export async function getServerSideProps() {
     try {
         const client = await clientPromise;
         const db = client.db("sample_mflix");
-
+  
         const movies = await db
             .collection("movies")
             .find({})
@@ -39,4 +39,4 @@ export async function getServerSideProps() {
     } catch (e) {
         console.error(e);
     }
-}
+  }
